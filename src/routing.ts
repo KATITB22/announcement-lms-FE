b08966ea8e7cbe67c83873cb6e73d0070ecafd28
IloveNooodles <mgarebaldhie80@gmail.com>
2022-07-19 00:44:42 +0700

    feat: add search page and filter

--- src/routing.ts
+++ src/routing.ts
@@ -3,6 +3,7 @@ import React, { LazyExoticComponent } from 'react';
 const Loading = React.lazy(() => import('./pages/Loading'));
 const HomePage = React.lazy(() => import('./pages/Homepage/Homepage'));
 const PageNotFound = React.lazy(() => import('./pages/PageNotFound'));
+const SearchPage = React.lazy(() => import('./pages/Searchpage/Searchpage'));
 interface PageRouting {
     title: string;
     path: string;
@@ -25,6 +26,11 @@ export const Routing: PageRouting[] = [
         title: 'Loading',
         path: '/loading',
         component: Loading,
+    },
+    {
+        title: 'Search',
+        path: '/search',
+        component: SearchPage,
     },
     PageNotFoundRouting,
 ];
