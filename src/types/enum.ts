b116a6ccf0813d60d6241c8954110b72a1062bdc
IloveNooodles <mgarebaldhie80@gmail.com>
2022-07-18 16:17:43 +0700

    feat: adding FontSize, Color and Font Constant

--- src/types/enum.ts
+++ src/types/enum.ts
@@ -1,4 +1,26 @@
-export enum StatusCode {}
-// SUCCESS = 1,
-// LOADING = 0,
-// FAILED = -1,
+/* eslint-disable no-unused-vars */
+export enum FontSize {
+    h1 = 96,
+    h2 = 60,
+    h3 = 48,
+    h4 = 34,
+    h5 = 24,
+    h6 = 20,
+    title = 16,
+    body = 14,
+    caption = 12,
+    overline = 10,
+}
+
+export enum Color {
+    Yellow = '#FFEBB0',
+    Orange = '#FFA06E',
+    LightOrange = '#F9D7AA',
+}
+
+export enum Font {
+    Heading = 'Magilio',
+    Subheading = 'Alegreya Semibold',
+    Body = 'Alegreya Sans Regular',
+    Caption = 'Alegreya Sans Thin',
+}
