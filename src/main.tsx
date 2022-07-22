949b8bce5c937581cd03b796dd589dca55a48a89
widyaput <13519105@std.stei.itb.ac.id>
2022-07-22 21:30:39 +0700

    feat: add header and footer

--- src/main.tsx
+++ src/main.tsx
@@ -3,29 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// const theme = extendTheme({
-//     fonts: {
-//         heading: "'Magilio', sans-serif",
-//         subheading: "'Alegreya Semibold', sans-serif",
-//         body: "'Alegreya Sans Regular', sans-serif",
-//         caption: "'Alegreya Sans Thin', sans-serif",
-//     },
-//     colors: {
-//         base: {
-//             footerText: '#511D05',
-//             footerBg: '#FFA06E',
-//             headerText: '#D27C2F',
-//             headerBg: '#FFEBB0',
-//             headerSelectedText: '#511D05',
-//         },
-//     },
-// });
+const test = ' heii';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        {/* <ChakraProvider theme={theme}>
-            <Fonts /> */}
         <App />
-        {/* </ChakraProvider> */}
     </React.StrictMode>
 );
