dcb7fa52fdf173d237993ebd90b8b9c681b0b15b
Fatih20 <fnri39@protonmail.com>
2022-08-04 19:54:47 +0700

    Enlarge the square on hover.

--- src/components/Rumpun.tsx
+++ src/components/Rumpun.tsx
@@ -2,8 +2,8 @@ import { RumpunProps } from '@/types/interface';
 
 const Rumpun: React.FC<RumpunProps> = ({ id, name }) => {
     return (
-        <div className="flex flex-col gap-2 justify-center items-center">
-            <div className="bg-LightBrown w-full aspect-square"></div>
+        <div className="flex flex-col gap-4 justify-center items-center">
+            <div className="bg-LightBrown w-full aspect-square hover:scale-105 transition-transform"></div>
             <h2 className="text-3xl">{name}</h2>
         </div>
     );
