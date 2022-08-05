e874cfbb5db8858b20e7c21c9eef612e8d11a1cd
ranjabi <fikriranjabi02@gmail.com>
2022-08-06 06:02:50 +0700

    fix eslint warning

--- src/components/Rumpun.tsx
+++ src/components/Rumpun.tsx
@@ -1,12 +1,10 @@
 import { RumpunProps } from '@/types/interface';
 
-const Rumpun: React.FC<RumpunProps> = ({ id, name }) => {
-    return (
-        <div className="flex flex-col gap-4 justify-center items-center">
-            <div className="bg-LightBrown w-full aspect-square hover:scale-105 transition-transform"></div>
-            <h2 className="text-3xl">{name}</h2>
-        </div>
-    );
-};
+const Rumpun: React.FC<RumpunProps> = ({ name }: { name: string }) => (
+    <div className="flex flex-col gap-4 justify-center items-center">
+        <div className="bg-LightBrown w-full aspect-square hover:scale-105 transition-transform" />
+        <h2 className="text-3xl">{name}</h2>
+    </div>
+);
 
 export default Rumpun;
