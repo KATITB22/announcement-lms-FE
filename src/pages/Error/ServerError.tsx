dbc863da54784e2901e3f509ad0b9dfa7121c813
Shafira Naya <shfrnaya@gmail.com>
2022-08-07 20:52:46 +0700

    style: responsive text

--- src/pages/Error/ServerError.tsx
+++ src/pages/Error/ServerError.tsx
@@ -11,7 +11,7 @@ const ServerError: React.FC<{}> = () => (
                 alt="Server Error"
                 className="w-[30rem] absolute left-[50%] bottom-[25%] translate-x-[-50%] -z-10"
             />
-            <p className="m-0 mt-20 mb-2 font-Heading text-h4 sm:text-h3 md:text-h3 text-DarkerOrange drop-shadow-errorStyle">
+            <p className="m-0 mt-24 pt-24 sm:mt-4 md:mt-4 mb-2 font-Heading text-h4 sm:text-h3 text-DarkerOrange drop-shadow-errorStyle">
                 Coba refresh halaman ini
             </p>
             <div className="bg-Orange rounded-full p-2 px-4 hover:shadow-sm">
