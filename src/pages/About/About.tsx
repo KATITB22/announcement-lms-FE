c59e3b89b72c7fb18197f4ae5e7cf01e67eafba5
Fatih20 <fnri39@protonmail.com>
2022-08-11 18:05:33 +0700

    Initial work on page transition.

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,6 +1,7 @@
 import { ABOUT_US_TITLE } from '@/types/constant';
 import oskmLogo from '@/assets/images/oskm-logo-text.svg';
 import { useToggle } from '@/hooks/useToggle';
+import PageTransition from '@/components/PageTransition';
 import AboutDetail from './AboutDetail';
 
 const About: React.FC<{}> = () => {
@@ -9,46 +10,48 @@ const About: React.FC<{}> = () => {
         'Pemasaran yang dulu dilakukan dengan menggunakan flyer, spanduk, koran, kini beralih ke media digital dan juga internet',
     ];
     return (
-        <div>
-            {isOn ? (
-                <AboutDetail toggle={toggle} />
-            ) : (
-                <div className="bg-gradient-to-b from-DarkOrange to-MediumBrown">
-                    <div className="container max-w-screen-lg min-h-screen mx-auto px-[3.75rem]">
-                        <p className="font-Heading text-[24px] text-center pt-16 pb-16">
-                            {ABOUT_US_TITLE}
-                        </p>
-                        <div className="relative flex flex-col items-center bg-MediumBrown opacity-70 rounded-[37px] pb-6 px-4 lg:bg-transparent text-center">
-                            <div className="hidden bg-MediumBrown w-60 h-40 rounded-xl absolute mt-16 lg:block">
-                                &nbsp;
-                            </div>
-                            <img
-                                className="relative -top-[4rem] -mb-[4rem] lg:w-96"
-                                src={oskmLogo}
-                                alt=""
-                            />
-                            {aboutContents.map((content: string) => (
-                                <p
-                                    key={`aboutcontent${aboutContents.indexOf(
-                                        content
-                                    )}`}
-                                    className="text-h6 md:text-[40px] lg:text-h3"
+        <PageTransition>
+            <div>
+                {isOn ? (
+                    <AboutDetail toggle={toggle} />
+                ) : (
+                    <div className="bg-gradient-to-b from-DarkOrange to-MediumBrown">
+                        <div className="container max-w-screen-lg min-h-screen mx-auto px-[3.75rem]">
+                            <p className="font-Heading text-[24px] text-center pt-16 pb-16">
+                                {ABOUT_US_TITLE}
+                            </p>
+                            <div className="relative flex flex-col items-center bg-MediumBrown opacity-70 rounded-[37px] pb-6 px-4 lg:bg-transparent text-center">
+                                <div className="hidden bg-MediumBrown w-60 h-40 rounded-xl absolute mt-16 lg:block">
+                                    &nbsp;
+                                </div>
+                                <img
+                                    className="relative -top-[4rem] -mb-[4rem] lg:w-96"
+                                    src={oskmLogo}
+                                    alt=""
+                                />
+                                {aboutContents.map((content: string) => (
+                                    <p
+                                        key={`aboutcontent${aboutContents.indexOf(
+                                            content
+                                        )}`}
+                                        className="text-h6 md:text-[40px] lg:text-h3"
+                                    >
+                                        {content}
+                                    </p>
+                                ))}
+                                <button
+                                    type="button"
+                                    onClick={toggle}
+                                    className="bg-MediumOrange text-white px-4 py-3 mt-8 rounded-[15px] drop-shadow-lg hover:drop-shadow-xl"
                                 >
-                                    {content}
-                                </p>
-                            ))}
-                            <button
-                                type="button"
-                                onClick={toggle}
-                                className="bg-MediumOrange text-white px-4 py-3 mt-8 rounded-[15px] drop-shadow-lg hover:drop-shadow-xl"
-                            >
-                                see more
-                            </button>
+                                    see more
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>
+        </PageTransition>
     );
 };
 
--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -7,6 +7,7 @@ import Pagination from '@/components/Pagination';
 import { PAGE_TITLE, SPONSOR_TITLE, MEDPAR_TITLE } from '@/types/constant';
 import BlogPost from '@/components/BlogPost';
 import { ErrorTypes } from '@/types/enum';
+import PageTransition from '@/components/PageTransition';
 import { listOfMedpar, sponsorMD } from '../Partnerpage/PartnerPage';
 import { LoadingSpecific } from '../Loading';
 import ErrorPage from '../ErrorPage';
@@ -40,63 +41,71 @@ const Homepage: React.FC<{}> = () => {
     ));
 
     return (
-        <div>
-            <div className="py-20 bg-Yellow">
-                <div className="container mx-auto px-6">
-                    <h1 className="text-4xl font-Heading font-bold mb-2 text-center">
-                        {PAGE_TITLE}
-                    </h1>
-                </div>
-            </div>
-            <div className="bg-gradient-to-b from-[#FF8952] to-[#F9DCB0] py-20 min-h-screen">
-                <div className="container max-w-screen-xl mx-auto px-[3.75rem]">
-                    <div className="my-4">
-                        <Pagination
-                            pagination={posts.meta.pagination}
-                            currentPage={page}
-                            setPage={setPage}
-                        />
-                    </div>
-                    <div className="flex justify-center">
-                        <div className="grid place-items-stretch lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-                            {blogPostElements}
-                        </div>
-                    </div>
-                    <div className="relative bg-LightBrown py-6 px-2 mt-16 rounded-lg flex justify-center">
-                        <h2 className="text-center font-Heading text-xl sm:text-2xl font-bold absolute top-[-20px] bg-LightBrown rounded-lg pt-1 px-3 drop-shadow-lg">
-                            {SPONSOR_TITLE}
-                        </h2>
-                        <div className="grid justify-items-center gap-4 p-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                            {sponsorMD.map((image: imgPartner) => (
-                                <a href={image.link} key={image.link}>
-                                    <img
-                                        alt={image.title}
-                                        src={image.imageUrl}
-                                        className={`w-[${image.type}] h-[${image.type}] object-cover`}
-                                    />
-                                </a>
-                            ))}
-                        </div>
-                    </div>
-                    <div className="relative bg-LightBrown py-6 px-2 mt-16 rounded-lg flex justify-center">
-                        <h2 className="text-center font-Heading text-xl sm:text-2xl font-bold absolute top-[-20px] bg-LightBrown rounded-lg pt-1 px-3 drop-shadow-lg">
-                            {MEDPAR_TITLE}
-                        </h2>
-                        <div className="grid justify-items-center gap-4 p-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                            {listOfMedpar.map((image: imgPartner) => (
-                                <a href={image.link} key={image.link}>
-                                    <img
-                                        alt={image.title}
-                                        src={image.imageUrl}
-                                        className={`w-[${image.type}] h-[${image.type}] object-cover`}
-                                    />
-                                </a>
-                            ))}
-                        </div>
+        <PageTransition>
+            <div>
+                <div className="py-20 bg-Yellow">
+                    <div className="container mx-auto px-6">
+                        <h1 className="text-4xl font-Heading font-bold mb-2 text-center">
+                            {PAGE_TITLE}
+                        </h1>
                     </div>
                 </div>
+                <div className="bg-gradient-to-b from-[#FF8952] to-[#F9DCB0] py-20 min-h-screen">
+                    <div className="container max-w-screen-xl mx-auto px-[3.75rem]">
+                        <div className="my-4">
+                            <Pagination
+                                pagination={posts.meta.pagination}
+                                currentPage={page}
+                                setPage={setPage}
+                            />
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="grid place-items-stretch lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
+                                {blogPostElements}
+                            </div>
+                        </div>
+                        <div className="relative bg-LightBrown py-6 px-2 mt-16 rounded-lg flex justify-center">
+                            <h2 className="text-center font-Heading text-xl sm:text-2xl font-bold absolute top-[-20px] bg-LightBrown rounded-lg pt-1 px-3 drop-shadow-lg">
+                                {SPONSOR_TITLE}
+                            </h2>
+                            <div className="grid justify-items-center gap-4 p-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                                {sponsorMD.map((image: imgPartner) => (
+                                    <a
+                                        href={image.link}
+                                        key={`${image.link} ${image.imageUrl}`}
+                                    >
+                                        <img
+                                            alt={image.title}
+                                            src={image.imageUrl}
+                                            className={`w-[${image.type}] h-[${image.type}] object-cover`}
+                                        />
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="relative bg-LightBrown py-6 px-2 mt-16 rounded-lg flex justify-center">
+                            <h2 className="text-center font-Heading text-xl sm:text-2xl font-bold absolute top-[-20px] bg-LightBrown rounded-lg pt-1 px-3 drop-shadow-lg">
+                                {MEDPAR_TITLE}
+                            </h2>
+                            <div className="grid justify-items-center gap-4 p-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                                {listOfMedpar.map((image: imgPartner) => (
+                                    <a
+                                        href={image.link}
+                                        key={`${image.link} ${image.imageUrl}`}
+                                    >
+                                        <img
+                                            alt={image.title}
+                                            src={image.imageUrl}
+                                            className={`w-[${image.type}] h-[${image.type}] object-cover`}
+                                        />
+                                    </a>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </PageTransition>
     );
 };
 
