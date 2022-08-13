5d60a162c01780cfed0a19cf76bb1bbfa66dc7fd
IloveNooodles <mgarebaldhie80@gmail.com>
2022-08-14 02:04:29 +0700

    fix: overlay cannot be clicked

--- src/components/VistockSearch.tsx
+++ src/components/VistockSearch.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import '../index.css';
 
 const VistockSearch: React.FC<{}> = () => (
-    <div className="absolute h-full w-full">
+    <div className="absolute h-full w-full z-[-1]">
         <div className="absolute search-bg h-full w-full" />
     </div>
 );
