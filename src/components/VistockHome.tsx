4fda3abd87e26369327296d40d749faa53fdc632
16721237@mahasiswa.itb.ac.id <16721237@mahasiswa.itb.ac.id>
2022-08-15 21:40:08 +0700

    fixed home-bg

--- src/components/VistockHome.tsx
+++ src/components/VistockHome.tsx
@@ -3,10 +3,7 @@ import '../index.css';
 
 const VistockHome: React.FC<{}> = () => (
     <div className="absolute h-full w-full -z-10">
-        <div
-            className="relative home-bg h-full w-full -z-10"
-            style={{ top: '-30vw', height: 'calc(100% + 30vw)' }}
-        />
+        <div className="relative home-bg h-full w-full -z-10" />
     </div>
 );
 
