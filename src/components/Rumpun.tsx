88fa07f99cfe4aed96d49c11fc0186e9a48d52a5
Alif Amirudin <96337290+Reigenleif@users.noreply.github.com>
2022-08-15 23:11:28 +0700

    updated defile (#86)

--- src/components/Rumpun.tsx
+++ src/components/Rumpun.tsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 const Rumpun: React.FC<RumpunProps> = ({ link, name }: RumpunProps) => (
     <div className="flex flex-col justify-center items-center">
         <Link
-            className="bg-LightBrown w-full aspect-video hover:scale-105 transition-transform"
+            className="bg-LightBrown w-full aspect-video hover:scale-105 transition-transform rounded-[30px] border-[10px] border-black"
             to={`/defile/${link}`}
         >
             <div className="flex flex-col text-center pt-20 ">
-                <h2 className="text-6xl">{name}</h2>
+                <h2 className="text-6xl font-[Magilio]">{name}</h2>
             </div>
         </Link>
     </div>
--- src/pages/TreasureHuntPage/TreasureHuntPage.tsx
+++ src/pages/TreasureHuntPage/TreasureHuntPage.tsx
@@ -39,6 +39,23 @@ const TreasureHuntPage = () => {
                 <h1 className="text-4xl font-Heading font-bold mb-2 text-center">
                     {available ? 'Defile Unit' : 'Selamat Datang'}
                 </h1>
+                <p className="text-center text-xl mx-[5em] my-[2em]">
+                    Haloo, teman-teman maba! <br />
+                    Selamat datang di DEFILE UNIT! Kalian telah berhasil
+                    memecahkan teka teki dan menemukan harta karun ini. <br />
+                    <br />
+                    Berikut terdapat beberapa rumpun dari unit-unit yang ada di
+                    ITB. Rumpun-rumpun ini adalah kategori dari berbagai unit di
+                    ITB yang serumpun, seperti BSO, Agama, Budaya, Seni,
+                    Olahraga, dan Pendidikan/Media/Kajian. Di dalamnya,
+                    teman-teman bisa melihat unit apa saja yang tergabung dalam
+                    kategori rumpun-rumpun yang ada. Silakan klik rumpunnya, dan
+                    saksikan beragam unit yang ada di ITB!
+                    <br />
+                    <br />
+                    Psst! Buat kalian yang masih kepo, pantengin terus OHU pada
+                    27 Agustus 2022 yaa!
+                </p>
             </div>
             {!available ? (
                 <div className="bg-gradient-to-b from-[#FF8952] to-[#F9DCB0] flex flex-col items-center justify-center py-5">
