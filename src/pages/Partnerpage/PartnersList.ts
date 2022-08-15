dc72d850bc67c92f9f3ed5931762c9d55fa685db
Albert <88728615+albert4183r7@users.noreply.github.com>
2022-08-15 16:04:54 +0700

    Update PartnersList.ts (#78)

--- src/pages/Partnerpage/PartnersList.ts
+++ src/pages/Partnerpage/PartnersList.ts
@@ -3,6 +3,15 @@ import { imgPartner } from '@/types/types';
 
 import BahanaFM from '@/assets/images/medpars/Bahana FM.png';
 import MMTCRADIO from '@/assets/images/medpars/MMTC RADIO.png';
+import InilahKoran from '@/assets/images/medpars/(XL) INILAH KORAN-min.png';
+import SeputarEvent from '@/assets/images/medpars/(S) Seputar Event-min.png';
+import OlimpiadeUpdate from '@/assets/images/medpars/(S) Olimpiade Update-min.png';
+import EventMahasiswa from '@/assets/images/medpars/(S) Info Event Mahasiswa Indonesia-min.png';
+import EventCampus from '@/assets/images/medpars/(S) Eventcampus-min.png';
+import EventList from '@/assets/images/medpars/(S) event.list-min.png';
+import EventNetwork from '@/assets/images/medpars/(S) Event Network-min.png';
+import EventBanget from '@/assets/images/medpars/(S) Event Banget Logo Black-min.png';
+
 
 const listOfMedpar: imgPartner[] = [
     {
@@ -15,6 +24,54 @@ const listOfMedpar: imgPartner[] = [
         title: 'MMTC Radio',
         type: typePartner.MD,
         imageUrl: MMTCRADIO,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Inilah Koran',
+        type: typePartner.LG,
+        imageUrl: InilahKoran,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Seputar Event',
+        type: typePartner.SM,
+        imageUrl: SeputarEvent,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Olimpiade Update',
+        type: typePartner.SM,
+        imageUrl: OlimpiadeUpdate,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Info Event Mahasiswa Indonesia',
+        type: typePartner.SM,
+        imageUrl: EventMahasiswa,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Eventcampus',
+        type: typePartner.SM,
+        imageUrl: EventCampus,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Event List',
+        type: typePartner.SM,
+        imageUrl: EventList,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Event Network',
+        type: typePartner.SM,
+        imageUrl: EventNetwork,
+        link: 'https://www.google.com', // link to detail post
+    },
+    {
+        title: 'Event Banget',
+        type: typePartner.SM,
+        imageUrl: EventBanget,
         link: 'https://www.google.com', // link to detail post
     },
 ];
