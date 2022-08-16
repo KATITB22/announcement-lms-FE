b5c410a05283f8530c3e8023d30c5b2f7da1b344
widyaput <13519105@std.stei.itb.ac.id>
2022-08-16 14:59:59 +0700

    feat: add merch list

---
+++ src/pages/Catalogue/MerchList.ts
@@ -0,0 +1,333 @@
+import { MerchPostType } from '@/types/interface';
+import Hat1 from '@/assets/images/merch/1.png';
+import Hat2 from '@/assets/images/merch/2.png';
+import Hat3 from '@/assets/images/merch/3.png';
+
+import Ganci1 from '@/assets/images/merch/4.png';
+import Ganci2 from '@/assets/images/merch/5.png';
+import Ganci3 from '@/assets/images/merch/6.png';
+import Ganci4 from '@/assets/images/merch/7.png';
+import Ganci5 from '@/assets/images/merch/8.png';
+import Ganci6 from '@/assets/images/merch/9.png';
+import Ganci7 from '@/assets/images/merch/10.png';
+import Ganci8 from '@/assets/images/merch/11.png';
+import Ganci9 from '@/assets/images/merch/12.png';
+import Ganci10 from '@/assets/images/merch/13.png';
+import Ganci11 from '@/assets/images/merch/14.png';
+import Ganci12 from '@/assets/images/merch/15.png';
+import Ganci13 from '@/assets/images/merch/16.png';
+import Ganci14 from '@/assets/images/merch/17.png';
+import Ganci15 from '@/assets/images/merch/18.png';
+
+import Gelang1 from '@/assets/images/merch/19.png';
+import Gelang2 from '@/assets/images/merch/20.png';
+import Gelang3 from '@/assets/images/merch/21.png';
+
+import Kipas from '@/assets/images/merch/22.png';
+
+import Tote1 from '@/assets/images/merch/23.png';
+import Tote2 from '@/assets/images/merch/24.png';
+import Tote3 from '@/assets/images/merch/25.png';
+
+import Pulpen from '@/assets/images/merch/26.png';
+
+import Pouch from '@/assets/images/merch/27.png';
+
+import Notebook1 from '@/assets/images/merch/28.png';
+import Notebook2 from '@/assets/images/merch/29.png';
+import Notebook3 from '@/assets/images/merch/30.png';
+
+import Hoodie1 from '@/assets/images/merch/31.png';
+import Hoodie2 from '@/assets/images/merch/32.png';
+import Hoodie3 from '@/assets/images/merch/33.png';
+
+import Kaos1 from '@/assets/images/merch/34.png';
+import Kaos2 from '@/assets/images/merch/35.png';
+import Kaos3 from '@/assets/images/merch/36.png';
+
+import Stiker1 from '@/assets/images/merch/37.png';
+import Stiker2 from '@/assets/images/merch/38.png';
+import Stiker3 from '@/assets/images/merch/39.png';
+import Stiker4 from '@/assets/images/merch/40.png';
+import Stiker5 from '@/assets/images/merch/41.png';
+import Stiker6 from '@/assets/images/merch/42.png';
+import Stiker7 from '@/assets/images/merch/43.png';
+import Stiker8 from '@/assets/images/merch/44.png';
+import Stiker9 from '@/assets/images/merch/45.png';
+import Stiker10 from '@/assets/images/merch/46.png';
+import Stiker11 from '@/assets/images/merch/47.png';
+import Stiker12 from '@/assets/images/merch/48.png';
+import Stiker13 from '@/assets/images/merch/49.png';
+import Stiker14 from '@/assets/images/merch/50.png';
+import Stiker15 from '@/assets/images/merch/51.png';
+import Stiker16 from '@/assets/images/merch/52.png';
+import Stiker17 from '@/assets/images/merch/53.png';
+import Stiker18 from '@/assets/images/merch/54.png';
+import Stiker19 from '@/assets/images/merch/55.png';
+
+import Meong1 from '@/assets/images/merch/56.png';
+import Meong2 from '@/assets/images/merch/57.png';
+import Meong3 from '@/assets/images/merch/58.png';
+import Meong4 from '@/assets/images/merch/59.png';
+import Meong5 from '@/assets/images/merch/60.png';
+import Meong6 from '@/assets/images/merch/61.png';
+import Meong7 from '@/assets/images/merch/62.png';
+import Meong8 from '@/assets/images/merch/63.png';
+import Meong9 from '@/assets/images/merch/64.png';
+import Meong10 from '@/assets/images/merch/65.png';
+import Meong11 from '@/assets/images/merch/66.png';
+
+import Pakdu1 from '@/assets/images/merch/67.png';
+import Pakdu2 from '@/assets/images/merch/68.png';
+import Pakdu3 from '@/assets/images/merch/69.png';
+
+import Mipa1 from '@/assets/images/merch/70.png';
+import Mipa2 from '@/assets/images/merch/71.png';
+import Mipa3 from '@/assets/images/merch/72.png';
+import Mipa4 from '@/assets/images/merch/73.png';
+
+import Jakang from '@/assets/images/merch/74.png';
+
+import Bundle1 from '@/assets/images/merch/75.png';
+import Bundle2 from '@/assets/images/merch/76.png';
+import Bundle3 from '@/assets/images/merch/77.png';
+import Bundle4 from '@/assets/images/merch/78.png';
+import Bundle5 from '@/assets/images/merch/79.png';
+import Bundle6 from '@/assets/images/merch/80.png';
+
+export const data: MerchPostType[] = [
+    {
+        title: 'Baju Angkatan 2022',
+        price: 'Rp 90.000',
+        url: 'https://bit.ly/BajuAngkatanITB2022',
+        images: [Jakang],
+    },
+    {
+        title: 'Bucket Hat',
+        price: 'Rp. 43.000',
+        url: 'https://tokopedia.link/nNVVR7c6vsb',
+        images: [Hat1, Hat2, Hat3],
+    },
+    {
+        title: 'Gantungan Kunci',
+        price: 'Rp. 12.000',
+        url: 'https://tokopedia.link/nXoQQtt6vsb',
+        images: [
+            Ganci1,
+            Ganci2,
+            Ganci3,
+            Ganci4,
+            Ganci5,
+            Ganci6,
+            Ganci7,
+            Ganci8,
+            Ganci9,
+            Ganci10,
+            Ganci11,
+            Ganci12,
+            Ganci13,
+            Ganci14,
+            Ganci15,
+        ],
+    },
+    {
+        title: 'Gelang',
+        price: 'Rp. 17.000',
+        url: 'https://tokopedia.link/yzLLfAv6vsb',
+        images: [Gelang1, Gelang2, Gelang3],
+    },
+    {
+        title: 'Hoodie',
+        price: 'Rp 150.000',
+        url: 'https://tokopedia.link/Ot9u4Qx6vsb',
+        images: [Hoodie1, Hoodie2, Hoodie3],
+    },
+    {
+        title: 'Kaos',
+        price: 'Rp. 90.000',
+        url: 'https://tokopedia.link/Fu9qGYz6vsb',
+        images: [Kaos1, Kaos2, Kaos3],
+    },
+    {
+        title: 'Kipas',
+        price: 'Rp. 15.000',
+        url: 'https://tokopedia.link/r4p4DxC6vsb',
+        images: [Kipas],
+    },
+    {
+        title: 'Notebook',
+        price: 'Rp 27.000',
+        url: 'https://tokopedia.link/EUi6CUF6vsb',
+        images: [Notebook1, Notebook2, Notebook3],
+    },
+    {
+        title: 'Pouch',
+        price: 'Rp. 27.000',
+        url: 'https://tokopedia.link/OjZFHuJ6vsb',
+        images: [Pouch],
+    },
+    {
+        title: 'Pulpen',
+        price: 'Rp. 7.000',
+        url: 'https://tokopedia.link/wjW37TL6vsb',
+        images: [Pulpen],
+    },
+    {
+        title: 'Sticker',
+        price: 'Rp. 5.000 | Rp. 13.000',
+        url: 'https://tokopedia.link/yaBYUFP6vsb',
+        images: [
+            Stiker1,
+            Stiker2,
+            Stiker3,
+            Stiker4,
+            Stiker5,
+            Stiker6,
+            Stiker7,
+            Stiker8,
+            Stiker9,
+            Stiker10,
+            Stiker11,
+            Stiker12,
+            Stiker13,
+            Stiker14,
+            Stiker15,
+            Stiker16,
+            Stiker17,
+            Stiker18,
+            Stiker19,
+        ],
+    },
+    {
+        title: 'Totebag',
+        price: 'Rp. 47.000',
+        url: 'https://tokopedia.link/OjZFHuJ6vsb',
+        images: [Tote1, Tote2, Tote3],
+    },
+    {
+        title: 'Bundle "Back to Campus"',
+        price: 'Rp. 57.000',
+        url: 'https://tokopedia.link/PlpNQk56vsb',
+        images: [Bundle1],
+    },
+    {
+        title: 'Bundle “Ready to Go”',
+        price: 'Rp. 190.000',
+        url: 'https://tokopedia.link/hSnFJv76vsb',
+        images: [Bundle2],
+    },
+    {
+        title: 'Bundle “Charming and Fashionable”',
+        price: 'Rp. 170.000',
+        url: 'https://tokopedia.link/tptWpo96vsb',
+        images: [Bundle3],
+    },
+    {
+        title: 'Bundle “Cute Little Stuffs”',
+        price: 'Rp. 48.000',
+        url: 'https://tokopedia.link/ccmB3Eb7vsb',
+        images: [Bundle4],
+    },
+    {
+        title: 'Bundle “Sticker Family”',
+        price: 'Rp. 24.000',
+        url: 'https://tokopedia.link/Nh2ILCf7vsb',
+        images: [Bundle5],
+    },
+    {
+        title: 'Bundle “Attractive in Class”',
+        price: 'Rp. 62.000',
+        url: 'https://tokopedia.link/Nh2ILCf7vsb',
+        images: [Bundle6],
+    },
+    {
+        title: 'Bundle Mathco x Chempro x Phiwiki',
+        price: 'Rp. 160.000',
+        url: 'https://tokopedia.link/FOQ3XQm7vsb',
+        images: [Mipa1, Mipa2, Mipa3, Mipa4],
+    },
+    {
+        title: 'Buku Meong Meong Matematika 1A',
+        price: 'Rp. 67.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong1],
+    },
+    {
+        title: 'Buku Meong Meong Matematika 1B',
+        price: 'Rp. 70.000',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong2],
+    },
+    {
+        title: 'Buku Meong Meong Matematika 2A',
+        price: 'Rp. 67.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong3],
+    },
+    {
+        title: 'Buku Meong Meong Matematika 2B',
+        price: 'Rp. 70.000',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong4],
+    },
+    {
+        title: 'Buku Meong Meong Matematika 2C',
+        price: 'Rp. 67.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong5],
+    },
+    {
+        title: 'Buku Meong Meong Matematika Bisnis 1',
+        price: 'Rp. 95.000',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong6],
+    },
+    {
+        title: 'Buku Meong Meong Matematika Bisnis 2',
+        price: 'Rp. 95.000',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong7],
+    },
+    {
+        title: 'Bundle 1 Buku Matematika Meong Meong',
+        price: 'Rp. 127.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong8],
+    },
+    {
+        title: 'Bundle 2 Buku Matematika Meong Meong',
+        price: 'Rp. 127.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong9],
+    },
+    {
+        title: 'Bundle 3 Buku Matematika Meong Meong',
+        price: 'Rp. 127.500',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong10],
+    },
+    {
+        title: 'Bundle 4 Buku Matematika Meong Meong',
+        price: 'Rp. 180.000',
+        url: 'https://tokopedia.link/yQZgE9t7vsb',
+        images: [Meong11],
+    },
+    {
+        title: 'Jas Laboratorium',
+        price: 'Rp. 82.500',
+        url: 'https://tokopedia.link/C3y43wA7vsb',
+        images: [Pakdu1],
+    },
+    {
+        title: 'Jurnal Praktikum',
+        price: 'Rp. 20.000',
+        url: 'https://tokopedia.link/I8Bem7D7vsb',
+        images: [Pakdu2],
+    },
+    {
+        title: 'Kalkulator',
+        price: 'Rp. 205.000',
+        url: 'https://tokopedia.link/VmmNsWF7vsb',
+        images: [Pakdu3],
+    },
+];
--- src/types/constant.ts
+++ src/types/constant.ts
@@ -24,6 +24,7 @@ export const PAGINATION_PER_PAGE = 5;
 export const PAGE_TITLE = '\uE000nno\uE070nce\uE063ent';
 export const SPONSOR_TITLE = 'S\uE064o\uE01Esore\uE03A B\uE05A:';
 export const MEDPAR_TITLE = '\uE023e\uE053ia Partner';
+export const CATALOGUE_TITLE = '\uE023\uE03b\uE066c\uE00F';
 export const REGEX_PARSE = /(\r\n|\n|\r)/gm;
 export const PARTNER_TITLE = '\uE096\uE00Ean\uE01As t\uE032';
 export const ABOUT_US_TITLE = '\uE000\uE050o\uE069\uE067 \uE038\uE076';
